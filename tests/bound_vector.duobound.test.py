c4a441eba4c9ec322dd2df1c1474f2bdf265e76e
hazen <hoyeungw@outlook.com>
2020-10-14 09:22:57 +0800

    update dependencies

--- tests/bound_vector.duobound.test.py
+++ tests/bound_vector.duobound.test.py
@@ -8,7 +8,7 @@ paramsList = {
     'asc_4': [0, 1, 2, 3],
     'desc_4': [3, 2, 1, 0],
     'misc': [False, 101, True, 103],
-    'misc2': [1, 't', None, 4, 'f'],
+    'misc2': [1, 't', None, 4, 'f', ' 3.5'],
     'tx_nums': ['244', 200, '306', '...', 'ant', 400, ],
     'tx_strs': 'comprehend how it\'s driven by animal spirits'.split(' '),
 }
